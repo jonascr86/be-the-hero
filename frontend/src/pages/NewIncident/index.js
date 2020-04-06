0f290f0328e29c758493fe761b42d39461259438
Jonas Correia da Rosa <jonascr86@gmail.com>
2020-04-05 23:39:25 -0300

    Criando Caso

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import logoImage from '../../assets/logo.svg';
 import './style.css';
 
+import api from '../../services/api';
+
 export default function NewIncident() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+    const ongId = localStorage.getItem('ongId');
+    const history = useHistory();
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        }
+
+        try {
+            
+            const response = await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            });
+            alert(`ID do seu novo caso é: ${response.data.id}`)
+            history.push('/profile');
+
+        } catch (error) {
+            alert(`Falha ao cadastrar novo caso: ${error}`)
+        }
+    }
     return (
-        <div className="new-incident-container">
+
+
+        < div className="new-incident-container" >
             <div className="content">
                 <section>
                     <img src={logoImage} alt="Be The Hero" />
@@ -21,10 +55,23 @@ export default function NewIncident() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Título do caso" />
-                    <textarea type="text" placeholder="Descrição" />
-                    <input placeholder="Valor em reais" />
+                <form onSubmit={handleNewIncident}>
+                    <input
+                        placeholder="Título do caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea
+                        type="text"
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input
+                        placeholder="Valor em reais"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <div className="group-button">
                         <button className="button-cancel">Cancelar</button>
@@ -32,6 +79,6 @@ export default function NewIncident() {
                     </div>
                 </form>
             </div>
-        </div>
+        </div >
     )
 }
