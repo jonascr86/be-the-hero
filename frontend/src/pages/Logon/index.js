10142b98a49ceae2d4bb6f39c59ed23e8983a276
Jonas Correia da Rosa <jonascr86@gmail.com>
2020-04-05 22:54:30 -0300

    Login, listar e deletar casos da ong, logout

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
 import './styles.css';
 import heroesImage from '../../assets/heroes.png'
 import logoImage from '../../assets/logo.svg'
+import api from '../../services/api';
 
 export default function Logon() {
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('session', { id });
+            localStorage.setItem('ongName', response.data.name);
+            localStorage.setItem('ongId', id);
+
+            history.push('/profile');
+            
+        } catch (error) {
+            alert(`Falha no login: ${error}`);
+        }
+
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImage} alt="Heroes" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}    
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link to="/register" className="back-link">
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,73 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower } from 'react-icons/fi'
 import { FiTrash2 } from 'react-icons/fi'
 
 import './style.css';
 import logoImage from '../../assets/logo.svg';
+import api from '../../services/api';
 
 export default function Profile() {
+
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+    const [incidents, setIncidents] = useState([]);
+    const history = useHistory();
+
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => {
+            setIncidents(response.data)
+        })
+    }, [ongId]);
+
+    async function handleDeleteIncident(id){
+        try {
+          await api.delete(`incidents/${id}`, {headers: {
+              Authorization: ongId,
+          }})
+        } catch (error) {
+            alert(`Erro ao deletar caso: ${error}`)
+        }
+
+        setIncidents(incidents.filter(incident => incident.id !== id ))
+    }
+
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImage} alt="Be The Hero" />
-                <span>Bem vindo a APAD</span>
+                <span>Bem vindo a {ongName}</span>
 
                 <Link className="button" to="/incident/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={handleLogout}>
                     <FiPower size={18} color="E02041" />
                 </button>
             </header>
 
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso tets</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição teste</p>
-                    <strong>Valor</strong>
-                    <p>R$ 200,00</p>
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.title}</p>
 
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso tets</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição teste</p>
-                    <strong>Valor</strong>
-                    <p>R$ 200,00</p>
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                        <strong>DESCRIÇÃO</strong>
+                        <p>{incident.description}</p>
 
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso tets</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição teste</p>
-                    <strong>Valor</strong>
-                    <p>R$ 200,00</p>
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                        <strong>Valor</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
+                        <button onClick={() => handleDeleteIncident(incident.id)}>
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
 
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso tets</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição teste</p>
-                    <strong>Valor</strong>
-                    <p>R$ 200,00</p>
-                    <button>
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
             </ul>
         </div>
     )
